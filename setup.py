b8cd1f7fddae3bfda03a0f982ee3f58dba32f097
Sung Kim <hunkim@gmail.com>
2022-05-26 15:04:49 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,6 @@ setup(
     author="Sung Kim",
     author_email="hunkim@gmail.com",
     py_modules=["streamlit_google_oauth"],
+    packages=["streamlit_google_oauth"]
     install_requires=["streamlit", "httpx-oauth", "typing-extensions"],
 )
